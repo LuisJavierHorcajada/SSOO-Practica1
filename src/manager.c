82038f454e3f3d731b6e6838beedadf81208b25b
Luis Javier <luisjavier.horcajada@alu.uclm.es>
2022-03-13 19:58:49 +0100

    Fixed concurrency issues between PB and PC.

--- src/manager.c
+++ src/manager.c
@@ -1,3 +1,4 @@
+#define _POSIX_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,27 +6,25 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-static pid_t pid_a;
-static pid_t pid_b;
-static pid_t pid_c;
+static pid_t pid_a = 0;
+static pid_t pid_b = 0;
+static pid_t pid_c = 0;
+static FILE *log;
 
 pid_t createProcess(char *);
-int createDirectory(FILE *);
-int createCopy(FILE *);
-int createExamRequirement(FILE *);
+int createDirectory();
+int createProcesses();
 double getAverageMark(pid_t, int *);
 void installSignalHandler();
 void signal_handler();
 
 int main(int argc, char *argv[]){
 
-    FILE *log;
     log = fopen("./log.txt", "a");
     installSignalHandler();
     fprintf(log, "\n******** Log del sistema ********\n");
-    createDirectory(log);
-    createCopy(log);
-    createExamRequirement(log);
+    createDirectory();
+    createProcesses();
     fclose(log);
     return EXIT_SUCCESS; 
 }
@@ -62,12 +61,13 @@ pid_t createProcessPipe(char * name, char * args){
     return pid;
 }
 
-int createDirectory(FILE *log){
+int createDirectory(){
 
     pid_a = createProcess("./exec/pa");
     int state;
     int cause;
     waitpid(pid_a, &state, 0);
+    pid_a = 0;
     cause = WIFEXITED(state);
     if (cause != 0){
         fprintf(log, "Creación de directorios finalizada.\n");
@@ -79,49 +79,48 @@ int createDirectory(FILE *log){
     }
 }
 
-int createCopy(FILE *log){
-    int state;
-    int cause;
+int createProcesses(){
 
-    pid_b = createProcess("./exec/pb");
-    waitpid(pid_b, &state, 0);
-    cause = WIFEXITED(state);
-    if(cause != 0){
-        fprintf(log, "Copia de modelos de examen, finalizada.\n");
-        return EXIT_SUCCESS;
-    }
-    else{
-        fprintf(log, "Copia de modelos de examen, ha tenido un error\n");
-        return EXIT_FAILURE;
-    }
-}
-
-int createExamRequirement(FILE *log){
-
-    int state;
-    int cause = 0;
+    int state, state2;
+    int cause, cause2;
     double average_mark = 0.0;
     int pipe2 [2];
     char wr_pipe [256];
     pipe(pipe2);
+    pid_t pb_hold, pc_hold;
 
     sprintf(wr_pipe, "%d", pipe2[1]);
 
+    pid_b = createProcess("./exec/pb");
     pid_c = createProcessPipe("./exec/pc", wr_pipe);
 
-    waitpid(pid_c, &state, 0);
-    cause = WIFEXITED(state);
-    if(cause != 0)
-        fprintf(log, "Creación de archivos con nota necesaria para alcanzar la nota de corte, finalizada.\n");
-    else{
-        fprintf(log, "Creación de archivos con nota necesaria para alcanzar la nota de corte, ha tenido un error.\n");
-        return EXIT_FAILURE;
-    }
+   do{
+       pb_hold = waitpid(pid_b, &state, WNOHANG);
+       pc_hold = waitpid(pid_c, &state2, WNOHANG);
 
-    average_mark = getAverageMark(pid_c, pipe2);
-    fprintf(log, "La nota media de la clase es: %0.2f .\n", average_mark);
-
-    return EXIT_SUCCESS;
+       if(pb_hold != 0){
+            pid_b = 0;
+            if(cause != 0){
+                fprintf(log, "Copia de modelos de examen, finalizada.\n");
+            }
+            else{
+                fprintf(log, "Copia de modelos de examen, ha tenido un error\n");
+                return EXIT_FAILURE;
+            }
+        }
+        if(pc_hold != 0){
+            pid_c = 0;
+            if(cause2 != 0){
+            fprintf(log, "Creación de archivos con nota necesaria para alcanzar la nota de corte, finalizada.\n");
+            average_mark = getAverageMark(pid_c, pipe2);
+            fprintf(log, "La nota media de la clase es: %0.2f .\n", average_mark);
+            }
+            else{
+                fprintf(log, "Creación de archivos con nota necesaria para alcanzar la nota de corte, ha tenido un error.\n");
+                return EXIT_FAILURE;
+            }
+        }
+   }while(pb_hold == 0 || pc_hold == 0);
 }
 
 double getAverageMark(pid_t pid, int *pipe){
@@ -137,11 +136,21 @@ double getAverageMark(pid_t pid, int *pipe){
 
 void installSignalHandler() {
     if (signal(SIGINT, signal_handler) == SIG_ERR) {
-        fprintf(stderr, "[MANAGER] Error installing signal handler: %s.\n");   
+        fprintf(stderr, "[MANAGER] Error installing signal handler.\n");   
         exit(EXIT_FAILURE);
     }
 }
 
 void signal_handler(int signo) {
+
+    if(pid_a != 0)
+        kill(pid_a, SIGINT);
+    if(pid_b != 0)
+        kill(pid_b, SIGINT);
+    if(pid_c != 0)
+        kill(pid_c, SIGINT);
+    fprintf(log, "Señal Ctrl + C recibida, el programa ha sido parado. \n");
+    fflush(log);
+    fclose(log);
     exit(EXIT_SUCCESS);
 }
